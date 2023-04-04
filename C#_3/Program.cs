e2ef847718f37a5a7e45dad8037461d8269e7ea6
Gorlitz <panfyorov.ivan@mail.ru>
2023-04-04 14:49:49 +0300

    Home_Work

---
+++ C#_3/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов
+и выводит их на экран.
+1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]*/
+
+Console.Clear();
+int[] num = new int[8];
+Console.Write("[");
+for (int i = 0; i < num.Length; i++)
+{
+    num [i] = new Random().Next(0, 100);
+    Console.Write(" " + Met (i) + " ");
+}
+Console.Write("]");
+int Met (int a)
+{
+    return num[a];
+}
